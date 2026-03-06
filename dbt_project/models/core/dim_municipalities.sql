22dfe21b1b9f41720c172cfd967e7d153511eff2
izugn <zugn.illes@gmail.com>
2026-03-06 07:37:00 +0100

    Refactoring: dim_municipalities.sql; fct_housing_prices.sql; fct_labout_market_regional.sql added.

--- dbt_project/models/core/dim_municipalities.sql
+++ dbt_project/models/core/dim_municipalities.sql
@@ -3,17 +3,34 @@
 }}
 
 with seed_regions as (
+
     select * from {{ ref('municipality_regions') }}
+
 ),
 
+-- Add the Danish DST API region name alongside the English name.
+-- This translation is needed because housing price tables (EJ56, EJ131)
+-- use Danish region names while the seed uses English names.
 final as (
+
     select
         municipality_code,
         municipality_name,
         region_code,
         region_name,
-        current_timestamp as _loaded_at
+
+        case region_name
+            when 'Capital Region of Denmark' then 'Region Hovedstaden'
+            when 'Region Zealand'            then 'Region Sjælland'
+            when 'Region of Southern Denmark' then 'Region Syddanmark'
+            when 'Central Denmark Region'    then 'Region Midtjylland'
+            when 'North Denmark Region'      then 'Region Nordjylland'
+        end                                 as region_name_dst,
+
+        current_timestamp                   as _loaded_at
+
     from seed_regions
+
 )
 
 select * from final
