691bf6ccc65b3b700025dc4b807ffbeaa495aee4
izugn <zugn.illes@gmail.com>
2026-03-11 19:38:18 +0100

    Issue 1: AFFORDABILITY_TIER Bug fixed; In mart_labour_housing_correlation.sql. Issue2: has_full_data, has_full_yesr togle added.

--- dbt_project/models/marts/mart_labour_housing_correlation.sql
+++ dbt_project/models/marts/mart_labour_housing_correlation.sql
@@ -17,10 +17,25 @@ with_affordability_tier as (
         annual_sales_count,
         price_to_income_ratio,
         case
+            when price_to_income_ratio is null  then null
             when price_to_income_ratio > 10 then 'HIGH_UNAFFORDABILITY'
             when price_to_income_ratio > 7  then 'MODERATE_UNAFFORDABILITY'
             else 'AFFORDABLE'
-        end as affordability_tier
+        end as affordability_tier,
+        case
+            when total_gross_unemployment is not null
+             and avg_annual_price_mdkk    is not null
+             and avg_disposable_income_dkk is not null
+             and price_to_income_ratio    is not null
+            then true
+            else false
+        end                                         as has_full_data,
+        -- True only when the year has all 4 quarters of price index data.
+        -- Filters out partial-year rows (e.g. 2025 with only 3 quarters).
+        case
+            when quarters_with_data = 4 then true
+            else false
+        end                                     as is_full_year
     from base
     where region_name is not null
 )
