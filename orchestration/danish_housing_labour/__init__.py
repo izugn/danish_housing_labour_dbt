9993c9c78021e005727e6711b9fd9c0304837d03
izugn <zugn.illes@gmail.com>
2026-03-15 11:17:32 +0100

    Fixing dagster dev.

--- orchestration/danish_housing_labour/__init__.py
+++ orchestration/danish_housing_labour/__init__.py
@@ -1,9 +1,9 @@
 from dagster import Definitions
-
-from .assets import all_assets
+from .assets import all_assets, dbt_resource
 from .schedules import daily_refresh_schedule
 
 defs = Definitions(
     assets=all_assets,
     schedules=[daily_refresh_schedule],
+    resources={"dbt": dbt_resource},
 )
